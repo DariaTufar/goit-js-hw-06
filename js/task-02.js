7168fd03e2ed9fe59d32106c3c487f6683e18abe
Daria Shenkevych <daria.shenkevych@gmail.com>
2022-09-18 23:46:45 +0300

    completed task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredients= document.getElementById(`ingredients`);
+
+ingredients.forEach ( function (element) {
+  const li = document.createElement( `li`);
+  li.textContent = element;
+  li.classList.add(`item`)
+  console.log(li)
+  listOfIngredients.append(li)
+  
+})
+
+
+
