9c31409623a1017b980c7fb97e13ee8699f11029
Daria Shenkevych <daria.shenkevych@gmail.com>
2022-09-19 23:35:03 +0300

    task 05 completed

---
+++ js/task-05.js
@@ -0,0 +1,19 @@
+// Write a script that, when typing in the input#name-input input (input event), substitutes its current value into span#name-output. If the input is empty, the span should display the "Anonymous" string.
+
+const refs = {
+input : document.getElementById(`name-input`),
+output : document.getElementById(`name-output`)
+}
+
+refs.input.addEventListener(`input`, handlerInput);
+
+function handlerInput(event) {
+if (event.currentTarget.value === `` ){
+    refs.output.textContent = `Anonymous`
+}
+else  {
+refs.output.textContent = event.currentTarget.value
+}
+}
+
+ 
